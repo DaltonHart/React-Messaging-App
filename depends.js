a799961986b1cdaea9c08b0b9c97341a11a5eb19
DaltonHart <DaltonVaren@gmail.com>
2019-04-30 16:51:30 -0700

    removed depemds js

--- depends.js
+++
@@ -1,2 +0,0 @@
-
-		emoji-mart firebase md5 mime-types moment react react-avatar-editor react-color react-dom react-redux react-router-dom react-scripts redux redux-devtools-extension semantic-ui-css semantic-ui-react uuid
