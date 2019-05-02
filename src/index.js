8016425e7f721e099b010b1b81a28a1adbc08700
DaltonHart <DaltonVaren@gmail.com>
2019-05-02 10:04:14 -0700

    set up redux for user auth

--- src/index.js
+++ src/index.js
@@ -1,33 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	withRouter
+} from 'react-router-dom';
+import firebase from './firebase';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { setUser } from './actions';
 
 import 'semantic-ui-css/semantic.min.css';
+import rootReducer from './reducers';
 
-const Root = () => (
-	<Router>
-		<Switch>
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-			<Route exact path="/" component={App} />
-			<Route
-				path="/*"
-				render={() => {
-					return <p>404</p>;
-				}}
-			/>
-		</Switch>
-	</Router>
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends Component {
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.props.setUser(user);
+				this.props.history.push('/');
+			}
+		});
+	}
+	render() {
+		return (
+			<Switch>
+				<Route path="/login" component={Login} />
+				<Route path="/register" component={Register} />
+				<Route exact path="/" component={App} />
+				<Route
+					path="/*"
+					render={() => {
+						return <p>404</p>;
+					}}
+				/>
+			</Switch>
+		);
+	}
+}
+
+const RootWithAuth = withRouter(
+	connect(
+		null,
+		{ setUser }
+	)(Root)
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<Router>
+			<RootWithAuth />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
