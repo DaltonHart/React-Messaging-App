e84f0d8a9822bbda90fdfc38f3a1dbed6be471d7
DaltonHart <DaltonVaren@gmail.com>
2019-05-05 14:19:16 -0700

    added messages form and styles

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessagesForm';
 
 class Messages extends Component {
 	render() {
