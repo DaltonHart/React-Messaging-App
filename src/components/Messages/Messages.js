9c8d3129f70be6333aa843a9732eb039c7f0cf42
DaltonHart <DaltonVaren@gmail.com>
2019-05-06 13:56:06 -0700

    added search functionality for messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -13,7 +13,10 @@ class Messages extends Component {
 		messages: [],
 		messagesLoading: true,
 		progressBar: false,
-		numUniqueUsers: ''
+		numUniqueUsers: '',
+		searchTerm: '',
+		searchLoading: false,
+		searchResults: []
 	};
 
 	componentDidMount() {
@@ -69,6 +72,28 @@ class Messages extends Component {
 
 	displayChannelName = channel => (channel ? `#${channel.name}` : '');
 
+	handleSearchChange = e => {
+		this.setState({ searchTerm: e.target.value, searchLoading: true }, () => {
+			this.handleSearchMessages();
+		});
+	};
+
+	handleSearchMessages = () => {
+		const channelMessages = [...this.state.messages];
+		const regex = new RegExp(this.state.searchTerm, 'gi');
+		const searchResults = channelMessages.reduce((acc, message) => {
+			if (
+				(message.content && message.content.match(regex)) ||
+				message.user.name.match(regex)
+			) {
+				acc.push(message);
+			}
+			return acc;
+		}, []);
+		this.setState({ searchResults });
+		setTimeout(() => this.setState({ searchLoading: false }), 1000);
+	};
+
 	render() {
 		const {
 			messagesRef,
@@ -76,19 +101,26 @@ class Messages extends Component {
 			user,
 			messages,
 			progressBar,
-			numUniqueUsers
+			numUniqueUsers,
+			searchTerm,
+			searchResults,
+			searchLoading
 		} = this.state;
 		return (
 			<>
 				<MessagesHeader
 					channelName={this.displayChannelName(channel)}
 					numUniqueUsers={numUniqueUsers}
+					handleSearchChange={this.handleSearchChange}
+					searchLoading={searchLoading}
 				/>
 
 				<Segment>
 					<Comment.Group
 						className={progressBar ? 'messages__progress' : 'messages'}>
-						{this.displayMessages(messages)}
+						{searchTerm
+							? this.displayMessages(searchResults)
+							: this.displayMessages(messages)}
 					</Comment.Group>
 				</Segment>
 
